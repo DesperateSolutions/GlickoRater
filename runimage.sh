42c8b779e9fde4a4530fcf14b725d45d5a5497bb
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2018-10-05 22:05:21 +0200

    Rename env from MONGODB_PORT_27017_TCP to MONGODB
    The previous is an old format use din tutum for service discovery

--- runimage.sh
+++ runimage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name glicko-rater --network=glickorater -p 3000:3000 -e MONGODB_PORT_27017_TCP_ADDR="glicko-mongo" -d desp/glickorater:local
+docker run --name glicko-rater --network=glickorater -p 3000:3000 -e MONGODB_ADDR="glicko-mongo" -d desp/glickorater:local
