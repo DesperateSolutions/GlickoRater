6edff358474abe9074d49798af05779538697926
Sigurhjörtur <sigurhjortur.snorrason@nagra.com>
2019-10-23 16:29:08 +0200

    Minor test cleanup

--- src/test/groovy/solutions/desperate/glicko/PlayerApiTest.groovy
+++ src/test/groovy/solutions/desperate/glicko/PlayerApiTest.groovy
@@ -13,7 +13,7 @@ class PlayerApiTest extends GlickoTestApp {
         client.httpPost("/league", makeLeague("league1")).code() == 204
         Response allLeagues = client.httpGet("/league")
         allLeagues.code() == 200
-        def league = new JsonSlurper().parse(allLeagues.body().byteStream()).find {it.name == "league1"}
+        def league = new JsonSlurper().parse(allLeagues.body().byteStream()).find { it.name == "league1" }
 
         when:
         def player1 = makePlayer("player1")
@@ -40,59 +40,22 @@ class PlayerApiTest extends GlickoTestApp {
 
         then:
         result.size() == 3
-        result.forEach { assert (it.name == "player1" || it.name == "player2" || it.name == "player3")}
+        result.forEach { assert (it.name == "player1" || it.name == "player2" || it.name == "player3") }
     }
-
-    def "Can get stats for player"() {
-        expect:
-        client.httpPost("/league", makeLeague("league1")).code() == 204
-        Response allLeagues = client.httpGet("/league")
-        allLeagues.code() == 200
-        def league = new JsonSlurper().parse(allLeagues.body().byteStream()).find {it.name == "league1"}
-        client.httpPost("/${league.id}/player", makePlayer("player1")).code() == 204
-        client.httpPost("/${league.id}/player", makePlayer("player2")).code() == 204
-        Response playerResponse = client.httpGet("/${league.id}/player")
-        playerResponse.code() == 200
-        def allPlayers = new JsonSlurper().parse(playerResponse.body().byteStream())
-        def player1 = allPlayers.find {it.name == "player1"}
-        def player2 = allPlayers.find {it.name == "player2"}
-
-        when:
-        Response gameResponse = client.httpPost("/${league.id}/game", makeGame(player1.id, player2.id, "1-0"))
-
-        then:
-        gameResponse.code() == 204
-
-        when:
-        def statsResponse = client.httpGet("/${league.id}/player/${player1.id}/stats")
-
-        then:
-        statsResponse.code() == 200
-
-        when:
-        def stats = new JsonSlurper().parse(statsResponse.body().byteStream())
-
-        then:
-        stats.wins == 1
-        stats.losses == 0
-        stats.draws == 0
-        stats.longestWinStreak == 1
-        stats.longestLossStreak == 0
-    }
-
+    
     def "Can get stats on games between players"() {
         expect:
         client.httpPost("/league", makeLeague("league1")).code() == 204
         Response allLeagues = client.httpGet("/league")
         allLeagues.code() == 200
-        def league = new JsonSlurper().parse(allLeagues.body().byteStream()).find {it.name == "league1"}
+        def league = new JsonSlurper().parse(allLeagues.body().byteStream()).find { it.name == "league1" }
         client.httpPost("/${league.id}/player", makePlayer("player1")).code() == 204
         client.httpPost("/${league.id}/player", makePlayer("player2")).code() == 204
         Response playerResponse = client.httpGet("/${league.id}/player")
         playerResponse.code() == 200
         def allPlayers = new JsonSlurper().parse(playerResponse.body().byteStream())
-        def player1 = allPlayers.find {it.name == "player1"}
-        def player2 = allPlayers.find {it.name == "player2"}
+        def player1 = allPlayers.find { it.name == "player1" }
+        def player2 = allPlayers.find { it.name == "player2" }
         def player1Stats = new JsonSlurper().parse(client.httpGet("/${league.id}/player/${player1.id}/stats").body().byteStream())
         def player2Stats = new JsonSlurper().parse(client.httpGet("/${league.id}/player/${player2.id}/stats").body().byteStream())
         player1Stats.wins == 0
@@ -106,11 +69,8 @@ class PlayerApiTest extends GlickoTestApp {
         player1Stats.ratingOverTime[0].rating == 1500
         player2Stats.ratingOverTime[0].rating == 1500
 
-         when:
-         Response gameResponse = client.httpPost("/${league.id}/game", makeGame(player1.id, player2.id, "1-0"))
-
-         then:
-         gameResponse.code() == 204
+        expect:
+        client.httpPost("/${league.id}/game", makeGame(player1.id, player2.id, "1-0")).code() == 204
 
         when:
         def statsResponse = client.httpGet("/${league.id}/player/${player1.id}/stats")
